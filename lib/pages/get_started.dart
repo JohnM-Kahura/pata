a6ba90efe6acfb7b82510c29d7eaa884398f9bc4
JohnM-Kahura <Johnmungai392@gmail.com>
2022-06-28 19:05:39 +0300

    rm default flutter code

---
+++ lib/pages/get_started.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class GetStarted extends StatelessWidget {
+  const GetStarted({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+          child: Column(
+        children: [
+          
+        ],
+      )),
+    );
+  }
+}

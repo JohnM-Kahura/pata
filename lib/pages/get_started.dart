ebd7e95f350d075c35a277b03afbf0f0bf05a9a5
JohnM-Kahura <Johnmungai392@gmail.com>
2022-09-16 21:17:57 +0300

    edit

--- lib/pages/get_started.dart
+++ lib/pages/get_started.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class GetStarted extends StatelessWidget {
   const GetStarted({super.key});
@@ -6,10 +7,30 @@ class GetStarted extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       body: SingleChildScrollView(
           child: Column(
         children: [
-          
+          // Container(
+          //   height: double.infinity,
+          //   width: double.infinity,
+          //   color: Colors.black.withOpacity(0.7),
+          // ),
+          Column(
+            children: [
+              SizedBox(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+
+                    Text('Shikisha loan with Goods',style: GoogleFonts.poppins(color: Colors.white,fontWeight: FontWeight.bold),),
+                     Text(
+                        'We solve your financial problemsby giving you a loan after offering an item as colateral')
+                  ],
+                ),
+              )
+            ],
+          ),
         ],
       )),
     );

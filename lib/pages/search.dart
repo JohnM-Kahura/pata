c08e49775fbc47bb1b6550a68c7c7f8f86010b50
JohnM-Kahura <Johnmungai392@gmail.com>
2022-09-18 19:35:02 +0300

    location search UI

---
+++ lib/pages/search.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:pata/pages/itemUpload.dart';
+import 'package:pata/widgets/customsearchbar.dart';
+
+class Searchpage extends StatefulWidget {
+  const Searchpage({super.key});
+
+  @override
+  State<Searchpage> createState() => _SearchpageState();
+}
+
+class _SearchpageState extends State<Searchpage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/background.jpg"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Container(
+          margin: const EdgeInsets.all(10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                children: [
+                  customSearchBar('search', context),
+                  Text(
+                    'search for the shop around you',
+                    style: GoogleFonts.poppins(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w400,
+                      fontSize: 30,
+                    ),
+                  ),
+                  Text(
+                    'example : we are located karen langata road',
+                    style: GoogleFonts.poppins(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w400,
+                      fontSize: 15,
+                    ),
+                  ),
+                ],
+              ),
+              Container(
+                margin: const EdgeInsets.only(bottom: 40),
+                child: TextButton(
+                  onPressed: () {
+                    //  get the search results
+                    Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context) => ItemUpload()));
+                  },
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.resolveWith(
+                          (states) => Colors.green)),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      'Request',
+                      style: GoogleFonts.poppins(
+                          fontWeight: FontWeight.w600,
+                          // fontSize: 22,
+                          color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}

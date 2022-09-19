26e04b3c7f1085b19f839f2df566c3f0644aacbc
JohnM-Kahura <Johnmungai392@gmail.com>
2022-09-19 13:20:25 +0300

    .

---
+++ lib/pages/loanApplication.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoanApplication extends StatefulWidget {
+  const LoanApplication({super.key});
+
+  @override
+  State<LoanApplication> createState() => _LoanApplicationState();
+}
+
+class _LoanApplicationState extends State<LoanApplication> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

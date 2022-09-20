4e465156bef786ba2c2c2a9c9c3fa6265823dac3
JohnM-Kahura <Johnmungai392@gmail.com>
2022-09-20 20:22:42 +0300

    rm unnecesarry dropdown

--- lib/pages/loanApplication.dart
+++ lib/pages/loanApplication.dart
@@ -60,29 +60,7 @@ class _LoanApplicationState extends State<LoanApplication> {
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(15),
             border: Border.all(color: Colors.green)),
-          child: DropdownButton<String>(
-      value: dropdownValue,
-      icon:const  Icon(Icons.arrow_drop_down_circle),
-      iconSize: 30,
-      elevation: 16,
-      style: const TextStyle(color: Colors.teal),
-      underline: Container(
-        height: 2,
-        color: Colors.black,
-      ),
-      onChanged: (newValue) {
-        setState(() {
-          dropdownValue = newValue!;
-        });
-      },
-      items: <String>['One', 'Two', 'Three', 'Four']
-          .map<DropdownMenuItem<String>>((String value) {
-        return DropdownMenuItem<String>(
-          value: value,
-          child: Text(value),
-        );
-      }).toList(),
-    )),
+          ),
 
       ],
     ),

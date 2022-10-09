10f1cc6efe99a388aaa244a4a43d9ff7197fdc71
JohnM-Kahura <Johnmungai392@gmail.com>
2022-10-09 18:28:05 +0300

    shared prefs toget started page

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,22 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
+import 'package:pata/utils/main_page.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'firebase_options.dart';
 import 'pages/get_started.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(const MyApp());
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  final prefs=await SharedPreferences.getInstance();
+  final showGetStarted=prefs.getBool('showGetStarted') ?? true;
+  
+  runApp( MyApp(showGetStarted:showGetStarted));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final bool showGetStarted;
+  const MyApp({super.key, required this.showGetStarted});
 
   // This widget is the root of your application.
   @override
@@ -22,7 +28,8 @@ class MyApp extends StatelessWidget {
         
         primarySwatch: Colors.blue,
       ),
-      home: const GetStarted(),
+      // the main page is just a wrapper so I can decide if there is a user on the device or not
+      home:showGetStarted==true? const GetStarted():const MainPage(),
     );
   }
 }

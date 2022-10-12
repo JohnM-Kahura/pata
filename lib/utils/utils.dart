4dd89a16e306fb7487689145265c9567c0ff9dd8
JohnM-Kahura <Johnmungai392@gmail.com>
2022-10-13 00:29:10 +0300

    custom snack bar

---
+++ lib/utils/utils.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Utils {
+  final messengerKey=GlobalKey<ScaffoldMessengerState>();
+ showSnackBar(String? text) {
+    if (text == null) return;
+
+    final snackBar = SnackBar(
+      content: Text(
+        text,
+        style: GoogleFonts.poppins(color: Colors.white),
+      ),
+      backgroundColor: Colors.red,
+      
+    );
+    messengerKey.currentState!..removeCurrentSnackBar()..showSnackBar(snackBar);
+  }
+}

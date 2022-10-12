20b550eeba80fe1b95b04d421170ef8b7101d920
JohnM-Kahura <Johnmungai392@gmail.com>
2022-10-13 00:26:51 +0300

    password reset functionality

---
+++ lib/auth/forgot_password.dart
@@ -0,0 +1,87 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:pata/utils/utils.dart';
+import 'package:pata/widgets/custom_text_feild.dart';
+
+class ForgotPassword extends StatelessWidget {
+  const ForgotPassword({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    TextEditingController emailController = TextEditingController();
+    Utils utils = Utils();
+    Future resetPassword() async {
+      showDialog(
+          context: context,
+          barrierDismissible: false,
+          builder: (context) => const Center(
+                child: CircularProgressIndicator(color: Colors.green),
+              ));
+      try {
+        await FirebaseAuth.instance
+            .sendPasswordResetEmail(email: emailController.text.trim());
+        utils.showSnackBar('Password Reset Email Sent');
+        Navigator.of(context).popUntil((route) => route.isFirst);
+      } on FirebaseAuthException catch (e) {
+        utils.showSnackBar(e.message);
+        Navigator.of(context).pop();
+      }
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Center(
+          child: Text(
+            'Reset Password',
+            style: GoogleFonts.poppins(
+              color: Colors.black,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'Receive an Email to rest your password',
+                style: GoogleFonts.poppins(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.symmetric(vertical: 20),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  border: Border.all(color: Colors.green),
+                ),
+                child: customTextFeild('Email', emailController),
+              ),
+              TextButton(
+                  onPressed: () async {
+                    await resetPassword();
+                  },
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.resolveWith(
+                          (states) => Colors.green)),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      'Reset Password',
+                      style: GoogleFonts.poppins(
+                          fontSize: 20,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600),
+                    ),
+                  )),
+            ]),
+      ),
+    );
+  }
+}

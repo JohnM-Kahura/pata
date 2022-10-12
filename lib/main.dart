51786f5d109888b0dc474890196cf7a0a35bf36d
JohnM-Kahura <Johnmungai392@gmail.com>
2022-10-13 00:26:09 +0300

    temp changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:pata/auth/login.dart';
 import 'package:pata/utils/main_page.dart';
+import 'package:pata/utils/utils.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'firebase_options.dart';
 import 'pages/get_started.dart';
@@ -21,15 +23,17 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    Utils utils=Utils();
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      scaffoldMessengerKey:utils.messengerKey,
       title: 'pata',
       theme: ThemeData(
         
         primarySwatch: Colors.blue,
       ),
       // the main page is just a wrapper so I can decide if there is a user on the device or not
-      home:showGetStarted==true? const GetStarted():const MainPage(),
+      home:showGetStarted==true? const GetStarted():const Login(),
     );
   }
 }

2942213a2e74f7f897dc21fa7224be75aef81689
JohnM-Kahura <Johnmungai392@gmail.com>
2022-10-13 18:40:27 +0300

    remove firebase options

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,12 @@ import 'package:pata/auth/login.dart';
 import 'package:pata/utils/main_page.dart';
 import 'package:pata/utils/utils.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'firebase_options.dart';
+// import 'firebase_options.dart';
 import 'pages/get_started.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await Firebase.initializeApp();//options: DefaultFirebaseOptions.currentPlatform
   final prefs=await SharedPreferences.getInstance();
   final showGetStarted=prefs.getBool('showGetStarted') ?? true;
   
